85f042d843d321b8e9dbec0a235a34e4942112db
Philipp Meyer <meyerphi@in.tum.de>
2013-08-30 17:11:13 +0200

    Updated z3-assignment-to-prolog-assignment.sh to accept any types

--- z3-assignment-to-prolog-assignment.sh
+++ z3-assignment-to-prolog-assignment.sh
@@ -9,4 +9,4 @@ sed 's/)  /)\
 /g' | \
 sed -e 's/[[:space:]][[:space:]]*/ /g' \
     -e 's/^ //' \
-    -e 's/(define-fun \([[:alnum:]][[:alnum:]]*\) () Int \([[:alnum:]][[:alnum:]]*\))/assignment(\1, \2)./'
+    -e 's/(define-fun \([[:alnum:]][[:alnum:]]*\) () [[:alnum:]][[:alnum:]]* \([[:alnum:]][[:alnum:]]*\))/assignment(\1, \2)./'
