efa5b74c08b965b0c9768edce8d66c03fe7b6de8
Ruslan Ledesma-Garza <ruslanledesmagarza@gmail.com>
2013-09-03 11:03:03 +0200

    Factored out unit tests.

--- run-tests.sh
+++ run-tests.sh
@@ -88,51 +88,54 @@ test-smt2-model-to-prolog-model petersons-alg/model-atheta1.smt2 petersons-alg/m
 # echo '- Testing construction of trap constraints C_theta for model A'
 # echo
 function test-trap-constraints {
-    set -e
-    sicstus -l "$sysdir"/src/trap-constraints.pl -- "$sysdir"/tests/petersons-alg/pp-petri-net.pl "$sysdir"/tests/petersons-alg/model-a1.pl 2>/dev/null >/tmp/constraints-ctheta1.smt2
-    sort "$sysdir"/tests/petersons-alg/constraints-ctheta1.smt2 >/tmp/constraints-ctheta1-exp.smt2
-    sort /tmp/constraints-ctheta1.smt2 >/tmp/constraints-ctheta1-out.smt2
-    diff /tmp/constraints-ctheta1-exp.smt2 /tmp/constraints-ctheta1-out.smt2
+    if (
+            set -e
+            sicstus -l "$sysdir"/src/trap-constraints.pl -- "$sysdir"/tests/$1 "$sysdir"/tests/$2 2>/dev/null >/tmp/constraints-ctheta.smt2
+            sort "$sysdir"/tests/$3 >/tmp/constraints-ctheta-exp.smt2
+            sort /tmp/constraints-ctheta.smt2 >/tmp/constraints-ctheta-out.smt2
+            diff /tmp/constraints-ctheta-exp.smt2 /tmp/constraints-ctheta-out.smt2
+        ); then
+        echo $3' ... PASS'
+    else
+        echo $3' ... FAILED'
+        exit 5
+    fi    
 }
-if test-trap-constraints; then
-    echo 'petersons-alg/constraints-ctheta1.pl ... PASS'
-else
-    echo 'petersons-alg/constraints-ctheta1.pl ... FAILED'
-    exit 5
-fi
-# test-trap-constraints petersons-alg/pp-petri-net.pl petersons-alg/model-a1.pl petersons-alg/constraints-ctheta1.smt2 # TODO: PASS ME
+test-trap-constraints petersons-alg/pp-petri-net.pl petersons-alg/model-a1.pl petersons-alg/constraints-ctheta1.smt2
 # test-trap-constraints petersons-alg/pp-petri-net.pl petersons-alg/model-a2.pl petersons-alg/constraints-ctheta2.smt2 # TODO: PASS ME
 
 # echo
 # echo '- Testing construction of constraint delta for C and A_theta'
 # echo
 function test-delta-constraint {
-    set -e
-    sicstus -l "$sysdir"/src/delta-constraint.pl -- "$sysdir"/tests/petersons-alg/model-atheta1.pl 2>/dev/null >/tmp/constraint-delta1-out.smt2
-    diff "$sysdir"/tests/petersons-alg/constraint-delta1.smt2 /tmp/constraint-delta1-out.smt2
+    if (
+            set -e
+            sicstus -l "$sysdir"/src/delta-constraint.pl -- "$sysdir"/tests/$1 2>/dev/null >/tmp/constraint-delta-out.smt2
+            diff "$sysdir"/tests/$2 /tmp/constraint-delta-out.smt2
+        ); then
+        echo $2' ... PASS'
+    else
+        echo $2' ... FAILED'
+        exit 6
+    fi
 }
-if test-delta-constraint; then
-    echo 'petersons-alg/constraint-delta1.smt2 ... PASS'
-else
-    echo 'petersons-alg/constraint-delta1.smt2 ... FAILED'
-    exit 6
-fi
-# test-delta-constraint petersons-alg/model-atheta1.pl petersons-alg/constraint-delta1.pl # TODO: PASS ME
-# test-delta-constraint petersons-alg/model-atheta2.pl petersons-alg/constraint-delta2.pl # TODO: PASS ME
+test-delta-constraint petersons-alg/model-atheta1.pl petersons-alg/constraint-delta1.smt2
+# test-delta-constraint petersons-alg/model-atheta2.pl petersons-alg/constraint-delta2.smt2 # TODO: PASS ME
 
 # echo
 # echo '- Testing construction of constraints C_n+1 for C_n and A_theta_n'
 # echo
 function test-succ-constraints {
-    set -e
-    "$sysdir"/src/succ-constraints.sh "$sysdir"/tests/petersons-alg/constraints-c0.smt2 "$sysdir"/tests/petersons-alg/constraint-delta1.smt2 >/tmp/constraints-c1-out.smt2
-    diff "$sysdir"/tests/petersons-alg/constraints-c1.smt2 /tmp/constraints-c1-out.smt2
+    if (
+            set -e
+            "$sysdir"/src/succ-constraints.sh "$sysdir"/tests/$1 "$sysdir"/tests/$2 >/tmp/constraints-c-out.smt2
+            diff "$sysdir"/tests/$3 /tmp/constraints-c-out.smt2
+        ); then
+        echo $3' ... PASS'
+    else
+        echo $3' ... FAILED'
+        exit 7
+    fi
 }
-if test-succ-constraints; then
-    echo 'petersons-alg/constraints-c1.smt2 ... PASS'
-else
-    echo 'petersons-alg/constraints-c1.smt2 ... FAILED'
-    exit 7
-fi
-# test-succ-constraints petersons-alg/constraints-c0.smt2 petersons-alg/constraint-delta1.smt2 petersons-alg/constraints-c1.smt2 # TODO: PASS ME
+test-succ-constraints petersons-alg/constraints-c0.smt2 petersons-alg/constraint-delta1.smt2 petersons-alg/constraints-c1.smt2
 # test-succ-constraints petersons-alg/constraints-c1.smt2 petersons-alg/constraint-delta2.smt2 petersons-alg/constraints-c2.smt2 # TODO: PASS ME
