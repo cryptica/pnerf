ac5b5c1f1085b6375757fa28808312e5579d5390
Philipp Meyer <meyerphi@in.tum.de>
2013-09-04 15:30:59 +0200

    Optimized dk-to-pp-dk.sh

--- benchmarks/dk-to-pp-dk.sh
+++ benchmarks/dk-to-pp-dk.sh
@@ -3,7 +3,7 @@
 # name transitions
 n=0
 while read line; do
-    if (echo $line | grep -e '->' 2>&1 >/dev/null); then
+    if [[ "$line" =~ ".*->.*" ]]; then
         n=$((n+1))
         echo "t$n : $line"
     else
