a3254fadfe17e5f9d209c5a62355cbd5491d0839
Philipp Meyer <meyerphi@in.tum.de>
2013-09-04 17:30:39 +0200

    Handled C-c in run_with_animation.sh

--- workspace/run_with_animation.sh
+++ workspace/run_with_animation.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 function animated_wait {
+  $* &
+  pid=$!
+  trap handle_ctrl_c INT
   tput civis
-  while (ps -p $1 >/dev/null); do
+  while (ps -p $pid >/dev/null); do
     for c in '|' '\' '-' '/'; do
       echo -n $c
       sleep .2s
@@ -12,4 +15,10 @@ function animated_wait {
   tput cnorm
 }
 
-($*) & (animated_wait $!)
+function handle_ctrl_c {
+  tput cnorm
+  kill $pid 2>/dev/null
+  wait $pid 2>/dev/null
+}
+
+animated_wait $*
