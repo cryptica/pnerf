bace2ff3dfea5a521f4e62842683f53e0ddf7d8f
Philipp Meyer <meyerphi@in.tum.de>
2013-09-10 15:53:51 +0200

    Refactored duplicate code in spec-to-pl-petri-net.sh

--- benchmarks/spec-to-pl-petri-net.sh
+++ benchmarks/spec-to-pl-petri-net.sh
@@ -39,18 +39,14 @@ sed -e '/^[[:blank:]]*$/D' \
     -e 's/\([[:alpha:]][[:alnum:]_]*\)[[:blank:]]*>=[[:blank:]]*\([[:digit:]][[:digit:]]*\)/(\1, \2)/g' \
     -e "s/[[:alpha:]][[:alnum:]_]*'[[:blank:]]*=//g" \
     -e 's/\([[:alpha:]][[:alnum:]_]*\)[[:blank:]]*+[[:blank:]]*\([[:digit:]][[:digit:]]*\)/(\1, \2)/g' \
+    -e 's/\([[:alpha:]][[:alnum:]_]*\)[[:blank:]]*-[[:blank:]]*\([[:digit:]][[:digit:]]*\)/(\1, -\2)/g' \
     -e 's/\([[:blank:]]*:[[:blank:]]*\(.*\)->[[:blank:]]*\)/\1\2, /' |\
 (
     n=0
     while read line; do
-      re1="(^.*->.*)\(([[:alpha:]][[:alnum:]_]*), ([[:digit:]]+)\)[[:blank:]]*,[[:blank:]]*(.*)\(\2, ([[:digit:]]+)\)[[:blank:]]*,[[:blank:]]*(.*)$"
-      while [[ $line =~ $re1 ]]; do
+      re="(^.*->.*)\(([[:alpha:]][[:alnum:]_]*), ([[:digit:]]+)\)[[:blank:]]*,[[:blank:]]*(.*)\(\2, (-?[[:digit:]]+)\)[[:blank:]]*,[[:blank:]]*(.*)$"
+      while [[ $line =~ $re ]]; do
         out_weight=$((BASH_REMATCH[3] + BASH_REMATCH[5]))
-        line=${BASH_REMATCH[1]}"("${BASH_REMATCH[2]}", "$out_weight"), "${BASH_REMATCH[4]}${BASH_REMATCH[6]}
-      done
-      re2="(^.*->.*)\(([[:alpha:]][[:alnum:]_]*), ([[:digit:]]+)\)[[:blank:]]*,[[:blank:]]*(.*)\2[[:blank:]]*-[[:blank:]]*([[:digit:]]+)[[:blank:]]*,[[:blank:]]*(.*)$"
-      while [[ $line =~ $re2 ]]; do
-        out_weight=$((BASH_REMATCH[3] - BASH_REMATCH[5]))
         if [[ $out_weight -gt 0 ]]; then
           line=${BASH_REMATCH[1]}"("${BASH_REMATCH[2]}", "$out_weight"), "${BASH_REMATCH[4]}${BASH_REMATCH[6]}
         else
