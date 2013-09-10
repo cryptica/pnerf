1d50b007128770661c31d93613055b459ca9c4e4
Philipp Meyer <meyerphi@in.tum.de>
2013-09-10 13:48:20 +0200

    Added negative versions of the boundedPN benchmarks found in mist repo

--- run-mist-on-benchmarks.sh
+++ run-mist-on-benchmarks.sh
@@ -5,28 +5,31 @@ if ! which mist >/dev/null; then
     exit 1
 fi
 
-for benchmark_dir in `find benchmarks -mindepth 2 -maxdepth 2 -type d`; do
+#for benchmark_dir in `find benchmarks -mindepth 2 -maxdepth 2 -type d`; do
+for benchmark_dir in `find benchmarks/found-in-mist-repo -mindepth 1 -maxdepth 1 -type d`; do
   rm -f $benchmark_dir/positive-mist.list
   rm -f $benchmark_dir/negative-mist.list
-  rm -f $benchmark_dir/unknown-mist.list
+  rm -f $benchmark_dir/error-mist.list
   rm -f $benchmark_dir/timeout-mist.list
   for spec_file in `find $benchmark_dir -name "*.spec"`; do
     echo $spec_file
     cat $spec_file | sed '/#.*/D' >/tmp/mist.in
-    if (set -o pipefail; timeout 10s mist --tsi /tmp/mist.in >$spec_file.mist.out); then
+    timeout 10s mist --tsi /tmp/mist.in >$spec_file.mist.out
+    result=$?
+    if [[ result -eq 0 ]]; then
       if (<$spec_file.mist.out grep 'TSI concludes safe' >/dev/null); then
           echo $spec_file >>$benchmark_dir/positive-mist.list
           echo "SAFE"
-      elif (<$spec_file.mist.out grep 'TSI concludes unsafe' >/dev/null); then
+      else
           echo $spec_file >>$benchmark_dir/negative-mist.list
           echo "UNSAFE"
-      else
-          echo $spec_file >>$benchmark_dir/unknown-mist.list
-          echo "UNKNOWN"
       fi
-    else
+    elif [[ result -ge 124 ]]; then
       echo $spec_file >>$benchmark_dir/timeout-mist.list
       echo "TIMEOUT"
+    else
+      echo $spec_file >>$benchmark_dir/error-mist.list
+      echo "ERROR"
     fi
   done
 done
