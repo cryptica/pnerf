32f7cca2ddd26cea81eeb4074ce10a2c6941e359
Philipp Meyer <meyerphi@in.tum.de>
2013-09-19 11:12:43 +0200

    Updated animation script

--- workspace/run_with_animation.sh
+++ workspace/run_with_animation.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 function animated_wait {
+  chars=('|' '\' '-' '/')
+  chars_len=${#chars[@]}
+  c=0
   fout="/tmp/animated_wait.out"
   line=1
-  $* > $fout &
+  echo $*
+  $* &
   pid=$!
   trap handle_ctrl_c INT
   tput civis
   while (ps -p $pid >/dev/null); do
     lines_count=`<$fout wc -l`
-    tail -n "+$line" $fout
+    tail -n +$line $fout
     line=$((lines_count+1))
-    for c in '|' '\' '-' '/'; do
-      echo -n $c
-      sleep .2s
-      tput cub1
-    done
+    
+    echo -n ${chars[$c]}
+    c=$(((c+1) % chars_len))
+    sleep .2s
+    tput cub1
   done
   tput cnorm
 }
@@ -24,7 +28,7 @@ function handle_ctrl_c {
   tput cnorm
   kill $pid 2>/dev/null
   wait $pid 2>/dev/null
-  tail -n "+$l" $fout
+  tail -n +$line $fout
 }
 
 animated_wait $*
