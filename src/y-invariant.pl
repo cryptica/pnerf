d2ed1a9966d80f2b30a2add64041e82f0cb6dadb
Philipp Meyer <meyerphi@in.tum.de>
2013-11-03 15:48:58 +0100

    Added pretty printing for invariants

--- src/y-invariant.pl
+++ src/y-invariant.pl
@@ -4,32 +4,43 @@
 :- dynamic target/2.       % target(PlaceId, TargetVal).
 
 :- use_module(library(lists)).
-:- use_module(library(aggregate)).
+:- use_module(library(ordsets)).
 
 :- ['load-pl-file.pl'].
 :- ['misc.pl'].
 
+format_y_component(P, Y) :-
+        ( Y = 0 -> true
+        ; Y = 1 -> format('~p', [P])
+        ; Y = -1 -> format('-~p', [P])
+        ; format('~p~p', [Y, P])
+        ).
+
 y_invariant :-
+        findall( (P, Y) , (
+                place(P, _, _),
+                assignment(P, Y),
+                Y > 0,
+                target(P, B),
+                B > 0
+        ), Xs ),
+        list_to_ord_set(Xs, Xord),
+        (   Xord = [(P0, Y0)|Xord1] ->
+            format_y_component(P0, Y0),
+            (   foreach((P1, Y1), Xord1)
+            do  print(' + '),
+                format_y_component(P1, Y1)
+            )
+        ;   print('0')
+        ),
         findall( S , (
                 place(P, _, _),
                 assignment(P, Y),
-                (init(P, M0) -> true; M0 = 0 ),
+                ( init(P, M0) -> true; M0 = 0 ),
                 S is Y * M0
         ), Ss ),
         sumlist(Ss, Ssum),
-        format('(assert (>= ~p (+', [Ssum]),
-        findall( _ , (
-                place(P, _, _),
-                assignment(P, Y),
-                target(P, B),
-                B > 0,
-                ( Y = 0 -> true
-                ; Y = 1 -> format(' ~p', [P])
-                ; Y = -1 -> format(' (- ~p)', [P])
-                ; format(' (* ~p ~p)', [Y, P])
-                )
-        ), _ ),
-        print(')))\n').
+        format(' <= ~p\n', [Ssum]).
 
 % Entry point
 :-      prolog_flag(argv, Argv),
