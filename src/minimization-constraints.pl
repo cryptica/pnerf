6b041251925e6ac8287bdc0552e69945f03c0c9c
Philipp Meyer <meyerphi@in.tum.de>
2013-11-21 12:58:47 +0100

    Improved method for minimization of invariants

--- src/minimization-constraints.pl
+++ src/minimization-constraints.pl
@@ -1,23 +1,31 @@
 :- dynamic place/3.        % place(Id, InTransitions, OutTransitions).
 :- dynamic assignment/2.   % assignment(Variable, Value).
-:- dynamic set_zero/1.     % set_zero(Place).
 
 :- ['load-pl-file.pl'].
 :- ['misc.pl'].
 
-next_constraint :-
-        place(P, _, _),
-        assignment(P, V),
-        V > 0,
-        \+ set_zero(P),
-        portray_clause(set_zero(P)).
+minimization_constraints :-
+        findall( _ , (
+             place(P, _, _),
+             format('(declare-fun b_~p () Int)\n', [P]),
+             format('(assert (implies (> ~p 0) (= b_~p 1)))\n', [P, P]),
+             format('(assert (implies (= ~p 0) (= b_~p 0)))\n', [P, P])
+        ), _ ),
+        findall(P, place(P, _, _), Ps),
+        findall(P, (
+             place(P, _, _),
+             assignment(P, V),
+             V > 0
+        ), PsV),
+        length(PsV, Count),
+        print('(assert (< '),
+        format_sum('b_~p', Ps),
+        format(' ~p))', [Count]).
 
 % Entry point
 :-      prolog_flag(argv, Argv),
         (   foreach(F, Argv)
         do  load_pl_file(F)
         ),
-        ( next_constraint ->
-          halt(0)
-        ; halt(1)
-        ).
+        minimization_constraints,
+        halt.
