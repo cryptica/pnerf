41e73136ffb36c4125c3e3d7560fb075efc8d37e
Philipp Meyer <meyerphi@in.tum.de>
2014-02-01 14:51:19 +0100

    Added version of paper draft with Filip's example

--- src/petri-net-to-dot.sh
+++ src/petri-net-to-dot.sh
@@ -8,6 +8,6 @@ sysdir=$(absolute_path "$0")
 
 input=$1
 
-sicstus -l "$sysdir"/input-file-to-petri-net.pl -- `pwd`/$input 2>/dev/null >/tmp/petri-net.pl
+sicstus -l "$sysdir"/input-file-to-petri-net.pl -- 0 `pwd`/$input 2>/dev/null >/tmp/petri-net.pl
 
 sicstus -l "$sysdir"/petri-net-to-dot.pl -- /tmp/petri-net.pl 2>/dev/null
